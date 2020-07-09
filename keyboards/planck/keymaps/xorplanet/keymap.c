5c88902cd4330cdd1877daf1570f242150255aba
kingogruffner <garrettruffner42@gmail.com>
2020-07-08 23:53:29 -0400

    half keymap update

--- keyboards/planck/keymaps/xorplanet/keymap.c
+++ keyboards/planck/keymaps/xorplanet/keymap.c
@@ -42,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | App  | Alt  |Lower |    Space    |Raise |   -  |   =  |   \  | Alt  |
+ * | Ctrl | GUI  | App  | Alt  |Lower |    Space    |Raise | nump |   =  |   \  | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-  KC_LCTL, KC_LALT, KC_APP, KC_LGUI,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_MINS, KC_EQL,  KC_BSLS, KC_RALT
+  KC_LCTL, KC_LGUI, KC_APP, KC_LALT,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_MINS, KC_EQL,  KC_BSLS, KC_RALT
 ),
 
 
@@ -57,18 +57,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |   {  |   }  | Del  | Home | PgDn | PgUp | End  |      |      |
+ * |      |      |      |   {  |   }  | Del  |      |   +  |   -  |   [  |   ]  |  ~   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |ISO ~ |ISO | |      |      |      |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   =  |   _  |   |  |   \  |  `   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Mute | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR, KC_LPRN, KC_RPRN,    KC_BSPC,
-  _______, _______, _______, KC_LCBR, KC_RCBR, KC_DEL,  KC_HOME, KC_PGDN,    KC_PGUP, KC_END,  _______,    _______,
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   S(KC_NUHS), KC_MPRV, KC_MNXT, S(KC_NUBS), _______,
-  _______, _______, _______, _______, _______, _______, _______, _______,    KC_VOLD, KC_VOLU, KC_MPLY,    KC_MUTE
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN,  KC_BSPC,
+  _______, _______, _______, KC_LCBR, KC_RCBR, KC_DEL,  _______, S(KC_EQL),  KC_MINS,    KC_LBRC, KC_RBRC,  KC_TILD,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_EQL,     S(KC_MINS), KC_PIPE, KC_BSLS,  KC_GRV,
+  _______, _______, _______, _______, _______, _______, _______, _______,    KC_VOLD,    KC_VOLU, KC_MPLY,  KC_MUTE
 ),
 
 
