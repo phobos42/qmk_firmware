2debf928eb2d8747289c316440ffcd20b9048872
kingogruffner <garrettruffner42@gmail.com>
2020-09-10 23:18:45 -0400

    update planck numpad

--- keyboards/planck/keymaps/xorplanet/keymap.c
+++ keyboards/planck/keymaps/xorplanet/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | App  | Alt  |Lower |    Space    |Raise | nump |   =  |   \  | Alt  |
+ * | Ctrl | GUI  | App  | Alt  |Lower |    Space    |Raise | nump |      |      | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_QUOT,
-  KC_LCTL, KC_LGUI, KC_APP, KC_LALT,  LOWER,   KC_SPC,  KC_SPC,  RAISE,  MO(_NUMP), KC_EQL,  KC_BSLS, KC_RALT
+  KC_LCTL, KC_LGUI, KC_APP, KC_LALT,  LOWER,   KC_SPC,  KC_SPC,  RAISE,  MO(_NUMP), XXXXXXX, XXXXXXX, KC_RCTL
 ),
 
 
@@ -72,15 +72,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |  +   |   -  |   4  |   5  |   6  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |   1  |   2  |   3  |
+ * |      |      |      |      |      |      |      |  *   |   \  |   1  |   2  |   3  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | NUMP |      |   0  |   .  |
+ * |      |      |      |      |      |             |      | NUMP |   0  |   .  |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMP] = LAYOUT_planck_grid(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   _______, KC_7,    KC_8,    KC_9,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_ENT,  KC_7,    KC_8,    KC_9,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_EQL), KC_MINS, KC_4,    KC_5,    KC_6,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   _______, KC_1,    KC_2,    KC_3,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ASTR,   KC_BSLS, KC_1,    KC_2,    KC_3,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX,   _______, KC_0,    KC_DOT, XXXXXXX
 ),
 
