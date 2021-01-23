63412610d8e62391165855a214a56ea9f5030915
phobos42 <garrettruffner42@gmail.com>
2021-01-23 14:35:21 -0500

    switch volume up and down

--- keyboards/planck/keymaps/phobos42/keymap.c
+++ keyboards/planck/keymaps/phobos42/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN,  KC_BSPC,
   _______, _______, _______, _______, KC_BSPC, KC_DEL,  _______, S(KC_EQL),  KC_MINS,    KC_LBRC, KC_RBRC,  KC_TILD,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_EQL,     S(KC_MINS), KC_PIPE, KC_BSLS,  KC_GRV,
-  _______, _______, _______, _______, _______, _______, _______, _______,    KC_VOLU,    KC_VOLD, KC_MPLY,  KC_MUTE
+  _______, _______, _______, _______, _______, _______, _______, _______,    KC_VOLD,    KC_VOLU, KC_MPLY,  KC_MUTE
 ),
 
 
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   _______, _______, _______, _______, _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_MPRV, KC_MNXT, KC_NUBS, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, KC_VOLD, KC_MPLY, KC_MUTE
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MUTE
 ),
 
 
